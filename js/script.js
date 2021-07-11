44293ae596251b5e11bf963d0d0506dd4ae99dc2
Denver-Meadows <meadows8200@gmail.com>
2021-07-11 14:49:11 -0400

    Nav scroll working

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
-// Make the nav bar work when clicked
+const nav = document.querySelector('.navigation__nav');
+const checkbox = document.querySelector('.navigation__checkbox')
+
+nav.addEventListener('click', function(e){
+  e.preventDefault()
+  const target = e.target.closest('.navigation__link')
+  if (!target) return;
+  const id = target.id
+
+  checkbox.checked = false;
+  document.querySelector(`#${id}-sec`).scrollIntoView({ behavior: 'smooth', block: 'center'});
+});
